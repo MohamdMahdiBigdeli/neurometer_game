9dcfaae836c33c88e0b5273715877eb20b364b24
Mohammad Mahdi Bigdeli <mohammadmahdi.bigdeli@yahoo.com>
2024-03-02 20:39:59 +0330

    connect startButton in 20,430 location to random endButton.

--- NeurometerGame/GameWindow.cs
+++ NeurometerGame/GameWindow.cs
@@ -43,17 +43,17 @@ namespace NeurometerGame
             //
             // next_way_location = first_way_location[index_startButton_location];
 
-            index_startButton_location = 0;
+            index_startButton_location = 2;
             startButton.Location = buttons_location[index_startButton_location];
             //buttons_location.RemoveAt(index_startButton_location);
-            endButton.Location = buttons_location[2];
+            endButton.Location = buttons_location[1];
 
             next_way_location = first_way_location[index_startButton_location];
             XX = next_way_location.X;
             YY = next_way_location.Y;
 
 
-            int i = 1, a;
+            int number_of_way = 0, a;
             PictureBox way;
             if (startButton.Location.X == 20)
             {
@@ -61,11 +61,13 @@ namespace NeurometerGame
                 {
                     while (true)
                     {
-                        if (i % 2 == 1)
+                        if (number_of_way % 2 == 0)
                         {
+                            number_of_way++;
+
                             way = new PictureBox();
                             way.BackColor = Color.Red;
-                            way.Name = i.ToString();
+                            way.Name = number_of_way.ToString();
                             a = 300;
                             if (a + XX > gamePanel.Width - endButton.Width - 20)
                                 a = gamePanel.Width - XX - 30;
@@ -75,15 +77,14 @@ namespace NeurometerGame
                             next_way_location.X += way.Width - width_way;
                             XX = way.Location.X + way.Width;
                             gamePanel.Controls.Add(way);
-                            // }
-
-                            i++;
                         }
                         else
                         {
+                            number_of_way++;
+
                             way = new PictureBox();
                             way.BackColor = Color.Green;
-                            way.Name = i.ToString();
+                            way.Name = number_of_way.ToString();
                             a = 200;
                             if (a + YY > gamePanel.Height - endButton.Height - 20)
                                 a = gamePanel.Height - YY - 30;
@@ -93,19 +94,17 @@ namespace NeurometerGame
                             next_way_location.Y += way.Height - width_way;
                             YY = way.Location.Y + way.Height;
                             gamePanel.Controls.Add(way);
-                            // }
-
-                            i++;
                         }
 
-                        if (i > 2 && (XX > endButton.Location.X && XX < endButton.Location.X + endButton.Width))
+                        if (number_of_way > 2 &&
+                            (XX - width_way > endButton.Location.X && XX < endButton.Location.X + endButton.Width))
                         {
                             switch (endButton.Location.Y)
                             {
                                 case 20:
                                     way = new PictureBox();
                                     way.BackColor = Color.Blue;
-                                    way.Name = i.ToString();
+                                    way.Name = number_of_way.ToString();
                                     way.Size = new Size(width_way, YY - endButton.Location.Y);
                                     next_way_location.Y = endButton.Location.Y;
                                     way.Location = next_way_location;
@@ -114,7 +113,7 @@ namespace NeurometerGame
                                 case 430:
                                     way = new PictureBox();
                                     way.BackColor = Color.Blue;
-                                    way.Name = i.ToString();
+                                    way.Name = number_of_way.ToString();
                                     way.Size = new Size(width_way, endButton.Location.Y - YY);
                                     next_way_location.Y = YY;
                                     way.Location = next_way_location;
@@ -125,14 +124,15 @@ namespace NeurometerGame
                             break;
                         }
 
-                        if (i > 2 && (YY > endButton.Location.Y && YY < endButton.Location.Y + endButton.Height))
+                        if (number_of_way > 2 &&
+                            (YY > endButton.Location.Y && YY < endButton.Location.Y + endButton.Height - width_way))
                         {
                             switch (endButton.Location.X)
                             {
                                 case 20:
                                     way = new PictureBox();
                                     way.BackColor = Color.Yellow;
-                                    way.Name = i.ToString();
+                                    way.Name = number_of_way.ToString();
                                     way.Size = new Size(XX - endButton.Location.X, width_way);
                                     next_way_location.X = endButton.Location.X;
                                     way.Location = next_way_location;
@@ -141,7 +141,103 @@ namespace NeurometerGame
                                 case 820:
                                     way = new PictureBox();
                                     way.BackColor = Color.Yellow;
-                                    way.Name = i.ToString();
+                                    way.Name = number_of_way.ToString();
+                                    way.Size = new Size(endButton.Location.X - XX, width_way);
+                                    next_way_location.X += width_way;
+                                    way.Location = next_way_location;
+                                    gamePanel.Controls.Add(way);
+                                    break;
+                            }
+
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    while (true)
+                    {
+                        if (number_of_way % 2 == 0)
+                        {
+                            number_of_way++;
+
+                            way = new PictureBox();
+                            way.BackColor = Color.Red;
+                            way.Name = number_of_way.ToString();
+                            a = 300;
+                            if (a + XX > gamePanel.Width - endButton.Width - 20)
+                                a = gamePanel.Width - XX - 30;
+                            way.Size = new Size(random.Next(width_way + 10, a), width_way);
+                            next_way_location.X += width_way;
+                            way.Location = next_way_location;
+                            next_way_location.X += way.Width - width_way;
+                            XX = way.Location.X + way.Width;
+                            gamePanel.Controls.Add(way);
+                        }
+                        else
+                        {
+                            number_of_way++;
+
+                            way = new PictureBox();
+                            way.BackColor = Color.Green;
+                            way.Name = number_of_way.ToString();
+                            a = 200;
+                            if (YY - a < 0 + endButton.Height + 20)
+                                a = YY - 30;
+                            way.Size = new Size(width_way, random.Next(width_way + 10, a));
+                            next_way_location.Y -= way.Height;
+                            way.Location = next_way_location;
+                            //next_way_location.Y += way.Height - width_way;
+                            YY = way.Location.Y;
+                            gamePanel.Controls.Add(way);
+                        }
+
+                        if (number_of_way > 2 &&
+                            (XX - width_way > endButton.Location.X && XX < endButton.Location.X + endButton.Width))
+                        {
+                            switch (endButton.Location.Y)
+                            {
+                                case 20:
+                                    way = new PictureBox();
+                                    way.BackColor = Color.Blue;
+                                    way.Name = number_of_way.ToString();
+                                    way.Size = new Size(width_way, YY - endButton.Location.Y);
+                                    next_way_location.Y = endButton.Location.Y;
+                                    way.Location = next_way_location;
+                                    gamePanel.Controls.Add(way);
+                                    break;
+                                case 430:
+                                    way = new PictureBox();
+                                    way.BackColor = Color.Blue;
+                                    way.Name = number_of_way.ToString();
+                                    way.Size = new Size(width_way, endButton.Location.Y - YY);
+                                    next_way_location.Y = YY;
+                                    way.Location = next_way_location;
+                                    gamePanel.Controls.Add(way);
+                                    break;
+                            }
+
+                            break;
+                        }
+
+                        if (number_of_way > 2 &&
+                            (YY > endButton.Location.Y && YY < endButton.Location.Y + endButton.Height - width_way))
+                        {
+                            switch (endButton.Location.X)
+                            {
+                                case 20:
+                                    way = new PictureBox();
+                                    way.BackColor = Color.Yellow;
+                                    way.Name = number_of_way.ToString();
+                                    way.Size = new Size(XX - endButton.Location.X, width_way);
+                                    next_way_location.X = endButton.Location.X;
+                                    way.Location = next_way_location;
+                                    gamePanel.Controls.Add(way);
+                                    break;
+                                case 820:
+                                    way = new PictureBox();
+                                    way.BackColor = Color.Yellow;
+                                    way.Name = number_of_way.ToString();
                                     way.Size = new Size(endButton.Location.X - XX, width_way);
                                     next_way_location.X += width_way;
                                     way.Location = next_way_location;
